f7feb65cae40778f77770f0f526f479cd01362ce
Student <example@example.com>
2018-05-08 14:42:08 -0700

    bundle exec rspec

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -10,18 +10,26 @@ class DestinationsController < ApplicationController
   end
 
   def create
-    @destination = Destination.create(destination_params)
-    json_response(@destination)
+    @destination = Destination.create!(destination_params)
+    json_response(@destination, :created)
   end
 
   def update
-    @destination = Destination.find(params[:id])
-    @destination.update(destination_params)
+    # @destination = Destination.find(params[:id])
+    # @destination.update(destination_params)
+    if @destination.update!(destination_params)
+      render status: 200, json: {
+        message: "Destination has been updated"
+      }
+    end
   end
 
   def destroy
     @destination = Destination.find(params[:id])
     @destination.destroy
+    render status: 200, json: {
+      message: "Destination has been removed"
+    }
   end
 private
   def destination_params
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,7 @@
 class Destination < ApplicationRecord
-  
+  # adding validations to these when a user adds them.. 
+  validates :country, presence: true
+  validates :city, presence: true
+  validates :description, presence: true
+  validates :best_coffee, presence: true
 end
