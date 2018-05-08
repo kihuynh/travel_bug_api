02ae6c800dcb545091fd7a85189a3d59c1ee14a2
Student <example@example.com>
2018-05-08 16:25:40 -0700

    token auth

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,4 +1,7 @@
 class DestinationsController < ApplicationController
+  TOKEN = "secret"
+  before_action :authenticate
+  
   def index
     @destinations = Destination.all
     json_response(@destinations)
@@ -35,4 +38,12 @@ private
   def destination_params
     params.permit(:country, :city, :description, :best_coffee)
   end
+
+  def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        # Compare the tokens in a time-constant manner, to mitigate
+        # timing attacks.
+        ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
+      end
+    end
 end
