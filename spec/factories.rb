e7a3bc1baf1eb786e5aef26d303600b4a03b99e0
Student <example@example.com>
2018-05-08 14:57:32 -0700

    test get request specs

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory(:destination) do
+    country('Amurrica')
+    city('Amurrica City')
+    description('It is fantastic')
+    best_coffee('Mcdonalds Coffee')
+  end
+end
---
+++ spec/requests/get_quotes_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "all destinations", :type => :request do
+  let!(:destinations) { FactoryBot.create_list(:destination, 15)}
+
+  before { get '/destinations'}
+
+  it 'returns all destinations' do
+    expect(JSON.parse(response.body).size).to eq 15
+  end
+
+  it 'status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
